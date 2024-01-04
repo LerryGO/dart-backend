ab7e3ee1e6af48093abbff284d42db3c3b4b5d04
Lerry Augusto <lerry.pirani@gmail.com>
2024-01-04 20:41:06 -0300

    Passagem de dados com Query Param e Router Param

--- bin/server_service.dart
+++ bin/server_service.dart
@@ -8,6 +8,22 @@ class ServerService {
     router.get('/', (Request request) {
       return Response(200, body: 'Primeira rota');
     });
+
+    // http://localhost:8080/ola/mundo/Lerry
+    // ola mundo
+    router.get('/ola/mundo/<usuario>', (Request req, String usuario) {
+      return Response.ok('Olá mundo $usuario',
+          headers: {'Content-Type': 'text/plain'});
+    });
+
+    // http://localhost:8080/query?name=Lerry&age=25
+    router.get('/query', (Request req) {
+      String? name = req.url.queryParameters['name'];
+      String? age = req.url.queryParameters['age'];
+      return Response.ok('Query é: $name, idade: $age ',
+          headers: {'Content-Type': 'text/plain'});
+    });
+
     return router.call;
   }
 }

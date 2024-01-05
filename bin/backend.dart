c58854490c8454fa7e78052d07feca2404333ec8
Lerry Augusto <lerry.pirani@gmail.com>
2024-01-04 22:10:20 -0300

    Iniciando o projeto e conhecendo Cascade e Pipeline

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,10 +1,22 @@
-import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf/shelf.dart';
 
-import 'server_service.dart';
+import 'api/blog_api.dart';
+import 'api/login_api.dart';
+import 'infra/custom_server.dart';
 
 void main() async {
+  var cascadeHandler =
+      Cascade().add(LoginApi().handler).add(BlogApi().handler).handler;
+
+  var handler =
+      Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
+
+  await CustomServer().initialize(handler);
+}
+
+/* void main() async {
   var serverService = ServerService();
   // Iniciando o servidor
   final server = await shelf_io.serve(serverService.handler, 'localhost', 8080);
   print('Nosso servidor foi iniciado http://localhost:8080');
-}
+} */

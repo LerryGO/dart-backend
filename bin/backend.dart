d38c27117557ba8a830a8209f73d9a5b76cd2fc3
Lerry Augusto <lerry.pirani@gmail.com>
2024-01-04 21:37:48 -0300

    Trabalhando com Jsons e Post Method

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,12 +1,10 @@
-import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
 import 'server_service.dart';
 
 void main() async {
-  var _server = ServerService();
+  var serverService = ServerService();
   // Iniciando o servidor
-  final server = await shelf_io.serve(
-      _server.handler, 'localhost', 8080);
+  final server = await shelf_io.serve(serverService.handler, 'localhost', 8080);
   print('Nosso servidor foi iniciado http://localhost:8080');
 }
--- bin/server_service.dart
+++ bin/server_service.dart
@@ -1,9 +1,13 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 class ServerService {
   Handler get handler {
     final router = Router();
+
+    // ------------ GET ---------------//
 
     router.get('/', (Request request) {
       return Response(200, body: 'Primeira rota');
@@ -22,6 +26,30 @@ class ServerService {
       String? age = req.url.queryParameters['age'];
       return Response.ok('Query é: $name, idade: $age ',
           headers: {'Content-Type': 'text/plain'});
+    });
+
+    // ------------ POST ---------------//
+
+    // BODY
+     /* {
+      "user": "admin",
+      "password": "123"
+     } */
+
+    router.post('/login', (Request req) async {
+      var result = await req.readAsString();
+      Map json = jsonDecode(result);
+
+      var user = json['user'];
+      var password = json['password'];
+
+      if (user == 'admin' && password == '123') {
+        // Se usuário == admin e senha == 123
+        return Response.ok('Bem vindo $user');
+      } else {
+        // Se não
+        return Response.forbidden('Acesso negado');
+      }
     });
 
     return router.call;

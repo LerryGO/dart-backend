be8a8e8f083d77587048e0e0431bcad7f1862d99
Lerry Augusto <lerry.pirani@gmail.com>
2024-01-04 23:07:04 -0300

    Criando APIs RESTful

--- bin/api/blog_api.dart
+++ bin/api/blog_api.dart
@@ -5,10 +5,29 @@ class BlogApi {
   Handler get handler {
     Router router = Router();
 
+    //  Listagem
     router.get('/blog/noticias', (Request req) {
       return Response.ok('Choveu hoje');
     });
 
+    // Nova noticia
+    router.post('/blog/noticias', (Request req) {
+      return Response.ok('Choveu hoje');
+    });
+
+    // Editar notícia
+    // /blog/noticias?id=1
+    router.put('/blog/noticias', (Request req) {
+      String? id = req.url.queryParameters['id'];
+      return Response.ok('Choveu hoje');
+    });
+
+    // Deletar notícia
+    // /blog/noticias?id=1
+    router.delete('/blog/noticias', (Request req) {
+      String? id = req.url.queryParameters['id'];
+      return Response.ok('Choveu hoje');
+    });
     return router.call;
   }
 }

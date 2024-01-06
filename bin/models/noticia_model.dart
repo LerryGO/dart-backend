e46b03c2fe2157e3b9e1079af9b0eb1634a7f635
Lerry Augusto <lerry.pirani@gmail.com>
2024-01-05 22:46:37 -0300

    Isolando as camadas de SERVICE e API

--- bin/models/noticia_model.dart
+++ bin/models/noticia_model.dart
@@ -1,13 +1,13 @@
 class NoticiaModel {
-  final int id;
+  final int? id;
   final String title;
   final String description;
   final String image;
   final DateTime createdAt;
   final DateTime? updatedAt;
-  
+
   NoticiaModel({
-    required this.id,
+    this.id,
     required this.title,
     required this.description,
     required this.image,
@@ -18,5 +18,28 @@ class NoticiaModel {
   @override
   String toString() {
     return 'NoticiaModel(id: $id, title: $title, description: $description, image: $image, createdAt: $createdAt, updatedAt: $updatedAt)';
+  }
+
+  factory NoticiaModel.fromJson(Map map) {
+    return NoticiaModel(
+      id: map['id'] ?? '',
+      title: map['title'],
+      description: map['description'],
+      image: map['image'],
+      createdAt: DateTime.fromMicrosecondsSinceEpoch(map['createdAt']),
+      updatedAt: map['updatedAt'] != null
+          ? DateTime.fromMicrosecondsSinceEpoch(map['updatedAt'])
+          : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'description': description,
+      'image': image,
+      'createdAt': createdAt.millisecondsSinceEpoch,
+    };
   }
 }

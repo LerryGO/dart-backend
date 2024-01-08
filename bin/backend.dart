b683172d51fc88068e5bf4dcc8ab410d170debc1
Lerry Augusto <lerry.pirani@gmail.com>
2024-01-08 20:45:42 -0300

    Organizando as Injeções de Dependências e Responsabilidades

--- bin/backend.dart
+++ bin/backend.dart
@@ -3,24 +3,21 @@ import 'package:shelf/shelf.dart';
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
-import 'infra/dependency_injector/dependency_injector.dart';
+
+import 'infra/dependency_injector/injects.dart';
 import 'infra/middleware_interception.dart';
-import 'infra/security/security_service.dart';
-import 'infra/security/security_service_imp.dart';
-import 'services/noticia_services.dart';
+
 import 'utils/custom_env.dart';
 
 void main() async {
   CustomEnv.fromFile('.env-dev');
 
-  final di = DependencyInjector();
-  di.register<SecurityService>(() => SecurityServiceImp(), isSingleton: true);
+  final di = Injects.initialize();
 
-  var securityService = di.get<SecurityService>();
 
   var cascadeHandler = Cascade()
-      .add(LoginApi(securityService).getHandler())
-      .add(BlogApi(NoticiaService()).getHandler(isSecurity: true))
+      .add(di.get<LoginApi>().getHandler())
+      .add(di.get<BlogApi>().getHandler(isSecurity: true))
       .handler;
 
   var handler = Pipeline()

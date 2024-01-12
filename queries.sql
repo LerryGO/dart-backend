2365d0de282a5fc8f9bcac96ea32534362779b77
Lerry Augusto <lerry.pirani@gmail.com>
2024-01-11 22:00:53 -0300

    Principais Comandos SQL

--- queries.sql
+++ queries.sql
@@ -19,3 +19,34 @@ UPDATE users SET name = 'João Gabriel', password = 'ABC123' WHERE id =3;
 
 # APAGA na tabela de users onde o id seja igual a 2
 DELETE FROM users WHERE id = 2;
+
+SELECT * FROM news
+
+INSERT INTO news (title, description, user_id) VALUES ("Notícia bombástica", "se chove a terra molha!!", 1);
+INSERT INTO news (title, description, user_id) VALUES ("Notícia Quente", "Hoje fez 30 graus", 3);
+
+UPDATE news SET title = "Notícia da Chuva" WHERE id = 1 AND user_id =1;
+
+SELECT * FROM news WHERE user_id = 1;
+
+# titulo noticia, descrição noticia, dt atualização noticia, nome do usuário que incluiu a noticia
+SELECT
+	n.title as "Título",
+    n.description as "Descrição",
+    n.updated_at as "Ultima atualização",
+    u.name as "Autor"
+FROM news n
+INNER JOIN users u on u.id = n.user_id
+WHERE u.name like "João%"
+AND n.id = 4;
+
+SELECT
+	n.title as "Título",
+    n.description as "Descrição",
+    n.updated_at as "Ultima atualização",
+    u.name as "Autor"
+FROM news n
+INNER JOIN users u on u.id = n.user_id
+ORDER BY n.updated_at DESC;
+
+SELECT * FROM news LIMIT 2;

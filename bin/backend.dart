d63bca52b742a05d1e656f7312e2e15e2d0bc2cc
Lerry Augusto <lerry.pirani@gmail.com>
2024-01-11 22:40:44 -0300

    Conectando o Projeto Dart ao MySQL

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,19 +1,30 @@
+import 'package:mysql1/mysql1.dart';
 import 'package:shelf/shelf.dart';
 
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
-
 import 'infra/dependency_injector/injects.dart';
 import 'infra/middleware_interception.dart';
-
 import 'utils/custom_env.dart';
 
 void main() async {
   CustomEnv.fromFile('.env-dev');
 
-  final di = Injects.initialize();
+  var connection = await MySqlConnection.connect(
+    ConnectionSettings(
+      host: await CustomEnv.get<String>(key: 'db_host'),
+      port: await CustomEnv.get<int>(key: 'db_port'),
+      user:await CustomEnv.get<String>(key: 'db_user') ,
+      password: await CustomEnv.get<String>(key: 'db_pass'),
+      db: await CustomEnv.get<String>(key: 'db_schema'),
+    ),
+  );
 
+  var result = await connection.query('SELECT * FROM users');
+  print(result);
+  
+  final di = Injects.initialize();
 
   var cascadeHandler = Cascade()
       .add(di.get<LoginApi>().getHandler())
